ce528824072702e10fc2b96fab07ea2bdd87055e
thtnv <thtnv9876@gmail.com>
2024-10-26 10:20:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mediaexample/MusicController.java
@@ -0,0 +1,10 @@
+package com.example.mediaexample;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+}
